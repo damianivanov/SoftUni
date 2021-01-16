4046c103d0d2d2c90707d89971da9f29366939ce
Damian <damian.iv@abv.bg>
2021-01-17 00:44:40 +0200

    .

---
+++ C# Web Basics/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace C__Web_Basics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
